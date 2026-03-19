2ed4867615fc590effd092ee20ecbd4d6c646afc
Stefano Sarmento <stefano_2007@hotmail.com>
2026-03-19 00:09:36 -0300

    Adicionando roles especificas para invalidar produtos

--- src/main/java/com/stefano/pedidos/endpoints/produtos/ProdutoController.java
+++ src/main/java/com/stefano/pedidos/endpoints/produtos/ProdutoController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -43,6 +44,7 @@ public class ProdutoController {
     }
 
     @PatchMapping("{produtoId}/inativar")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_GERENCIADOR')")
     public ResponseEntity<Void> inativarProduto(@PathVariable("produtoId") Long produtoId) {
 
         produtoService.inativarProduto(produtoId);
